10215231dbd0e69b0f5ff3d6afb267927642a065
NicoPerso <janvier.nico@gmail.com>
2018-05-18 18:29:37 +0100

    Testing sap.m.TextArea

--- App.controller.js
+++ App.controller.js
@@ -1,15 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
+    "sap/m/MessageToast", 'sap/ui/model/json/JSONModel'
+ ], function (Controller, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("HelloWorld.App", {
-       onShowHello : function () {
-          MessageToast.show("Hello World");
-          this.onTest();
-       },
-       onTest : function() {
+        onInit: function () {
+			var oModel = new JSONModel({data: {}});
+			this.getView().setModel(oModel);
+        },
+        onShowHello : function () {
+            MessageToast.show("Hello World");
+            this.onTest();
+        },
+        onTest : function() {
             MessageToast.show("Test");
-       }
+        }
     });
  });
